e51c116889cb4ef76d928b0d7597796777024f1a
Louis Beaumont <louis.beaumont@gmail.com>
2023-03-29 08:07:52 +0000

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import EmbedbaseClient from './EmbedbaseClient'
 
 export { default as EmbedbaseClient } from './EmbedbaseClient'
-
+export type {
+  BatchAddDocument, SearchOptions, ClientSearchData,
+  ClientAddData, ClientContextData, ClientDatasets
+} from './types'
 /**
  * Creates a new Embedbase Client.
  */
